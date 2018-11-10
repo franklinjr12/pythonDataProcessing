3730bf2dfe1f22a00162f5ac565b50dd0d151b2d
franklinjr12 <franklinjunior12@hotmail.com>
2018-11-10 14:38:35 -0200

    data add

--- processamentoDados.py
+++ processamentoDados.py
@@ -1,6 +1,43 @@
+#======================================================================================================================
+# --- Processa toda uma pasta de dados ---
+
+# media = 0
+# maximum = 0
+# minimum = 256
+# i = 1
+
+# for filename in os.listdir("rodando"):
+# 	myFile = open("rodando\\"+str(filename), 'r')
+
+# 	numbersList = fileToIntList(myFile)
+
+# 	x = domain(0, len(numbersList)-1, 1)
+# 	y = numbersList
+# 	actualMean = mean(y)
+# 	media = media + actualMean
+# 	i = i+1
+# 	if actualMean > maximum:
+# 		maximum = actualMean
+# 	if actualMean < minimum:
+# 		minimum = actualMean
+
+# print("Resultados: ")
+
+# print("Media: " + str(media/i))
+
+# print("Maximo: " + str(maximum))
+
+# print("minimo: " + str(minimum))
+
+#======================================================================================================================
+
+
+
+
 #read data from a file in the format: int\nint\nint\n...
 
 import math
+import os
 import matplotlib.pyplot
 from plotting import *
 
@@ -45,19 +82,33 @@ def minimum(numbersList):
 
 
 
-myFile = open("coletasDispositivo\\semSensor1540954526", 'r')
+myFile = open("rodando\\noiteToda1540993982", 'r')
 
 numbersList = fileToIntList(myFile)
+
+media = mean(numbersList)	
+i = 0
+for e in numbersList:
+	numbersList[i] = numbersList[i] - media
+	i = i+1
 
 
 x = domain(0, len(numbersList)-1, 1)
 y = numbersList
 
-
 matplotlib.pyplot.figure(1)
+
 myPlot(x,y)
 
+print("Rodando: ")
+
 print("Media: " + str(mean(y)))
+
+print("Maximo: " + str(maximum(y)))
+
+matplotlib.pyplot.show()
+
+
 
 
 ##myFile = open("coletasDispositivo\\comSensorSemCarga1539052914", 'r')
@@ -72,19 +123,3 @@ print("Media: " + str(mean(y)))
 ##y = numbersList
 ##matplotlib.pyplot.figure(3)
 ##myPlot(x,y)
-
-
-matplotlib.pyplot.show()
-
-
-print(maximum(y))
-
-
-
-##print(numbersList)
-##
-##print("Mean of the numbers: " + str(mean(numbersList)))
-##
-##print("Larger number: " + str(maximum(numbersList)))
-##
-##print("Lesser number: " + str(minimum(numbersList)))
